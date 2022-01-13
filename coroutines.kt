0ea61333b2e91479d494e5e34c7d020540ea7093
Anushka Singh <anushkasingh117733@gmail.com>
2022-01-13 16:02:01 +0530

    Update coroutines.kt

--- coroutines.kt
+++ coroutines.kt
@@ -1,1 +1,14 @@
+import kotlinx.coroutines.*
 
+fun main() {
+   val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+   repeat(3) {
+       GlobalScope.launch {
+           println("${Thread.currentThread()} has started")
+           for (i in states) {
+               println("${Thread.currentThread()} - $i")
+               delay(5000)
+           }
+       }
+   }
+}
