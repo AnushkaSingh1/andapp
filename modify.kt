0d4c6aee72140c544ad58f6e311ffbd15d4abee5
Anushka Singh <anushkasingh117733@gmail.com>
2022-01-14 19:57:45 +0530

    Update modify.kt

--- modify.kt
+++ modify.kt
@@ -10,3 +10,19 @@ dependencies {
 
     ...
 }
+
+
+
+Manifests/AndroidManifest.xml
+
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.example.android.marsphotos">
+
+    <!-- In order for our app to access the Internet, we need to define this permission. -->
+    <uses-permission android:name="android.permission.INTERNET" />
+
+    <application
+        ...
+    </application>
+
+</manifest>
