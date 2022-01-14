9f19c7f1ba5e233993507994fd9f34e5ad7e5f05
Anushka Singh <anushkasingh117733@gmail.com>
2022-01-14 19:56:51 +0530

    Create modify.kt

---
+++ modify.kt
@@ -0,0 +1,12 @@
+build.gradle(Module : MarsPhotos.app)
+
+dependencies {
+    ...
+    // Moshi
+    implementation 'com.squareup.moshi:moshi-kotlin:1.9.3'
+
+    // Retrofit with Moshi Converter
+    implementation 'com.squareup.retrofit2:converter-moshi:2.9.0'
+
+    ...
+}
